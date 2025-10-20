67e6efeffbc2ab61cd3bb3c422c3f8fc8f677e60
Pablo <araujopablo95@gmail.com>
2025-10-20 09:15:54 -0300

    Automapper

---
+++ ApiPeliculas/Modelos/DTOs/CategoriaDto.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ApiPeliculas.Modelos.DTOs
+{
+    public class CategoriaDto
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "El nombre es obligatorio")]
+        [MaxLength(100, ErrorMessage = "El numero maximo de caracteres es de 100.")]
+        public string Nombre { get; set; }
+        [Required]
+        public DateTime FechaCreacion{ get; set; }
+    }
+}
---
+++ ApiPeliculas/PeliculasMappers/PeliculasMapper.cs
@@ -0,0 +1,13 @@
+﻿using ApiPeliculas.Modelos.DTOs;
+using AutoMapper;
+
+namespace ApiPeliculas.PeliculasMappers
+{
+    public class PeliculasMapper : Profile
+    {
+        public PeliculasMapper() {
+            CreateMap<CategoriaRepositorio, CategoriaDto>().ReverseMap();
+            CreateMap<CategoriaRepositorio, CrearCategoriaDto>().ReverseMap();
+        }
+    }
+}
