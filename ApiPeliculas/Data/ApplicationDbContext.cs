248d32b3f12467a7d281439d5368312739c5be7b
Pablo <araujopablo95@gmail.com>
2025-10-27 19:22:19 -0300

    Modelo y migracion BD Usuario

--- ApiPeliculas/Data/ApplicationDbContext.cs
+++ ApiPeliculas/Data/ApplicationDbContext.cs
@@ -11,5 +11,6 @@ namespace ApiPeliculas.Data
 
         public DbSet<Categoria> Categorias { get; set; }
         public DbSet<Pelicula> Pelicula { get; set; }
+        public DbSet<Usuario> Usuario { get; set; }
     }
 }
---
+++ ApiPeliculas/Modelos/Usuario.cs
@@ -0,0 +1,11 @@
+﻿namespace ApiPeliculas.Modelos
+{
+    public class Usuario
+    {
+        public int Id { get; set; }
+        public string NombreUsuario { get; set; }
+        public string Nombre { get; set; }
+        public string Password { get; set; }
+        public string Rol { get; set; }
+    }
+}
