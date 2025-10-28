ff9769e06e2ae96e2f21c82cfb0b5a8a986312e3
Pablo <araujopablo95@gmail.com>
2025-10-28 12:57:34 -0300

    Metodo Login en Repositorio JWT config

--- ApiPeliculas/Repositorio/UsuarioRepositorio.cs
+++ ApiPeliculas/Repositorio/UsuarioRepositorio.cs
@@ -2,6 +2,11 @@
 using ApiPeliculas.Modelos;
 using ApiPeliculas.Modelos.DTOs;
 using ApiPeliculas.Repositorio.IRepositorio;
+using Microsoft.IdentityModel.Tokens;
+using System.Globalization;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 using XSystem.Security.Cryptography;
 
 namespace ApiPeliculas.Repositorio
@@ -9,11 +14,12 @@ namespace ApiPeliculas.Repositorio
     public class UsuarioRepositorio : IUsuarioRepositorio
     {
         private readonly ApplicationDbContext _db; // Contexto de base de datos (inyección de dependencias)
-
+        private String secretKey;
         // Constructor que recibe el contexto y lo asigna al campo privado
-        public UsuarioRepositorio(ApplicationDbContext db)
+        public UsuarioRepositorio(ApplicationDbContext db, IConfiguration config)
         {
             _db = db;
+            secretKey = config.GetValue<string>("ApiSettings:secretKey");
         }
         public Usuario GetUsuario(int usuarioId)
         {
@@ -35,9 +41,43 @@ namespace ApiPeliculas.Repositorio
             return true;
         }
 
-        public Task<UsuarioLoginRespuestaDto> Login(UsuarioLoginDto usuarioLoginDto)
+        public async Task<UsuarioLoginRespuestaDto> Login(UsuarioLoginDto usuarioLoginDto)
         {
-            throw new NotImplementedException();
+            var passwordEncriptado = obtenermd5(usuarioLoginDto.Password);
+            var usuario = _db.Usuario.FirstOrDefault(userDB => userDB.Password == passwordEncriptado && 
+            userDB.NombreUsuario.ToLower() == usuarioLoginDto.NombreUsuario.ToLower());
+            //Validaamos si el usuario no existe con la combinacion de usuario y password correcta
+            if (usuario == null)
+            {
+                return new UsuarioLoginRespuestaDto()
+                {
+                    Token = "",
+                    Usuario = null
+                };
+            }
+            //Aca existe el usuario entonces podemos procesar el Login
+            var manejadorToken = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes(secretKey);
+
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new Claim[]
+                {
+                    new Claim(ClaimTypes.Name, usuario.NombreUsuario.ToString()),
+                    new Claim(ClaimTypes.Role, usuario.Rol.ToString()),
+                }),
+                Expires = DateTime.UtcNow.AddDays(7),
+                SigningCredentials = new(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+            };
+
+            var token = manejadorToken.CreateToken(tokenDescriptor);
+            UsuarioLoginRespuestaDto usuarioLoginRespuestaDto = new UsuarioLoginRespuestaDto()
+            {
+                Token = manejadorToken.WriteToken(token),
+                Usuario = usuario
+            };
+
+            return usuarioLoginRespuestaDto;
         }
 
         public async Task<Usuario> Registro(UsuarioRegistroDto usuarioRegistroDto)
